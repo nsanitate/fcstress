94b0ce6224192c12751ecb9620313843f4ecd946
Nicola Sanitate <n.sanitate@gmail.com>
2015-10-12 13:58:50 +0200

    Stress test

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,30 @@
 'use strict';
+(function(_, moment) {
+	$(document).ready(function() {
 
-$(document).ready(function() {
+		var end, start = moment().startOf('week');
+		var events = [];
 
-	var cal = $('#calendar').fullCalendar({
-    	defaultView: 'agendaWeek',
-    	slotDuration: '00:10:00',
-    	allDayDefault: false,
-    	editable: true
-    });
+		_.times(7, function() {
+			_.times(287, function() {
+				end = moment(start).add(5,'minutes');
+				events.push({
+					title: 'Event',
+					start: start,
+					end: end
+				});
+				start = moment(end);
+			});
+			start.add(1,'days');
+			start.startOf('day');
+		});
 
-	cal.fullCalendar('select', new Date('2015/05/21 07:00:00'));
-	cal.fullCalendar('select', new Date('2015/05/21 12:00:00'));
-});
+		$('#calendar').fullCalendar({
+	    	defaultView: 'agendaWeek',
+	    	slotDuration: '00:05:00',
+	    	allDayDefault: false,
+	    	editable: true,
+	    	events: events
+	    });
+	});
+})(window._, window.moment);
